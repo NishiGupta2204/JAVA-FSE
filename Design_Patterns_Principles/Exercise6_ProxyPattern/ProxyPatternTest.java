f04450497eb6da88665b52a151eacecd436599be
Nishi Gupta <nishigupta2204@gmail.com>
2026-06-21 16:43:49 +0530

    Exercise 6

---
+++ Design_Patterns_Principles/Exercise6_ProxyPattern/ProxyPatternTest.java
@@ -0,0 +1,15 @@
+public class ProxyPatternTest {
+
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("photo.jpg");
+
+        System.out.println("First call:");
+        image.display();
+
+        System.out.println();
+
+        System.out.println("Second call:");
+        image.display();
+    }
+}
