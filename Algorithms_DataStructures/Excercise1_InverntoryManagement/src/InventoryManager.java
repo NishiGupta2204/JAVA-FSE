7eb78cb46a7733fc1c5722d7ffd56df4fa36aada
Nishi Gupta <134848342+NishiGupta2204@users.noreply.github.com>
2026-06-21 10:08:42 +0530

    Implement inventory management methods in InventoryManager

--- Algorithms_DataStructures/Excercise1_InverntoryManagement/src/InventoryManager.java
+++ Algorithms_DataStructures/Excercise1_InverntoryManagement/src/InventoryManager.java
@@ -1,1 +1,46 @@
+import java.util.HashMap;
 
+public class InventoryManager {
+
+    HashMap<Integer, Product> inventory = new HashMap<>();
+
+    public void addProduct(Product product) {
+        inventory.put(product.getProductId(), product);
+    }
+
+    public void updateProduct(int productId, int quantity, double price) {
+
+        if (inventory.containsKey(productId)) {
+
+            Product product = inventory.get(productId);
+
+            product.setQuantity(quantity);
+            product.setPrice(price);
+
+            System.out.println("Product updated successfully.");
+        } else {
+            System.out.println("Product not found.");
+        }
+    }
+
+    public void deleteProduct(int productId) {
+
+        if (inventory.containsKey(productId)) {
+
+            inventory.remove(productId);
+
+            System.out.println("Product deleted successfully.");
+        } else {
+            System.out.println("Product not found.");
+        }
+    }
+
+    public void displayProducts() {
+
+        for (Product product : inventory.values()) {
+
+            System.out.println(product);
+            System.out.println();
+        }
+    }
+}
