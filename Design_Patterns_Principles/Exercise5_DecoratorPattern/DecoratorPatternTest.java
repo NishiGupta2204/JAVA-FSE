c15da2ac901dad38320879df5e0025c32a3f9670
Nishi Gupta <nishigupta2204@gmail.com>
2026-06-21 16:34:39 +0530

    Exercise 5

---
+++ Design_Patterns_Principles/Exercise5_DecoratorPattern/DecoratorPatternTest.java
@@ -0,0 +1,12 @@
+public class DecoratorPatternTest {
+
+    public static void main(String[] args) {
+
+        Notifier notifier = new EmailNotifier();
+
+        notifier = new SMSNotifierDecorator(notifier);
+        notifier = new SlackNotifierDecorator(notifier);
+
+        notifier.send("Hello, User!");
+    }
+}
