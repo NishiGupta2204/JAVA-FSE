a37740972686d4621f7605c91715b8ad5651b9a3
Nishi Gupta <134848342+NishiGupta2204@users.noreply.github.com>
2026-06-21 10:09:18 +0530

    Implement inventory management functionality in Main
    The main method initializes the InventoryManager, adds products, updates a product, and deletes another product while displaying inventory details.

--- Algorithms_DataStructures/Excercise1_InverntoryManagement/src/Main.java
+++ Algorithms_DataStructures/Excercise1_InverntoryManagement/src/Main.java
@@ -1,1 +1,25 @@
+public class Main {
 
+    public static void main(String[] args) {
+
+        InventoryManager manager = new InventoryManager();
+
+        manager.addProduct(new Product(101, "Laptop", 15, 65000));
+        manager.addProduct(new Product(102, "Mouse", 50, 500));
+        manager.addProduct(new Product(103, "Keyboard", 30, 1200));
+
+        System.out.println("Inventory Details");
+        System.out.println();
+
+        manager.displayProducts();
+
+        manager.updateProduct(102, 60, 550);
+
+        manager.deleteProduct(103);
+
+        System.out.println("\nUpdated Inventory");
+        System.out.println();
+
+        manager.displayProducts();
+    }
+}
